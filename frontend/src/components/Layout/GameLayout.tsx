23799906bbc5894031904e663cb5151578d2bcf2
ukotaro <umedakotaro1209@gmail.com>
2023-04-22 15:43:56 +0900

    レイアウト作成 Fixes #13

--- frontend/src/components/Layout/GameLayout.tsx
+++ frontend/src/components/Layout/GameLayout.tsx
@@ -1,28 +1,35 @@
-import { ReactElement } from "react";
-import Head from "next/head";
+import { ReactElement } from "react"
+import Head from "next/head"
 
 export const metadata = {
-  title: 'AI',
-  description: 'AI',
+  title: "AI",
+  description: "AI",
 }
 
 type LayoutProps = {
   readonly children: ReactElement
 }
 
-const Layout = ({ children }: LayoutProps) => {
+const GameLayout = ({ children }: LayoutProps) => {
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <main>
-          <div className='container mx-auto p-2 pt-16'>
-            {children}
-          </div>
-        </main>
+      <main>
+        <div className="layout">{children}</div>
+      </main>
+
+      <style jsx>{`
+        .layout {
+          background-size: cover;
+          background-position: center;
+          background-image: url("/放課後の教室.jpeg");
+          height: 100vh;
+        }
+      `}</style>
     </>
   )
 }
 
-export default Layout;
+export default GameLayout
