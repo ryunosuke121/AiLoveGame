8ac8ebcf7b665b4681e84e9565ee44817280876e
ryunosuke121 <ryunosuke.ito@horizon-cg.com>
2023-04-22 16:21:21 +0900

    女の子の画像生成APIを作成する Fixes #1

--- backend/api/routers/image.py
+++ backend/api/routers/image.py
@@ -1,7 +1,22 @@
 from fastapi import APIRouter
+from fastapi.responses import StreamingResponse
+import os
+import openai
+from dotenv import load_dotenv
+load_dotenv()
 
+router = APIRouter()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 router = APIRouter()
 
 @router.get("/images")
 async def create_img(prompt: str):
-    pass
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size='1024x1024',
+    )
+
+    image_url = response['data'][0]['url']
+    print(image_url)
+    return image_url
