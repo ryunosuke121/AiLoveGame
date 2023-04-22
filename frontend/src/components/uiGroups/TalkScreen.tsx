f8e88df99ea303f321c5aa7468ea8b7aab4c3a53
ukotaro <umedakotaro1209@gmail.com>
2023-04-23 00:08:19 +0900

    結果画面作成 Fixes #29

--- frontend/src/components/uiGroups/TalkScreen.tsx
+++ frontend/src/components/uiGroups/TalkScreen.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 export default function TalkScreen(props: any) {
-  const { clickSetEnd, text, clickSetConfession, name } = props
+  const { clickSetEnd, text, clickSetConfession, name, placeholder, talkButton } = props
   const [isInput, setIsInput] = useState(true)
   const completeInput = () => {
     setIsInput(!isInput)
@@ -11,22 +11,36 @@ export default function TalkScreen(props: any) {
       <div>
         <div className="flex justify-between items-end">
           <img src="ジェシー.png" alt="ジェシー" />
-          <button className="mr-40 mb-5" onClick={clickSetEnd || clickSetConfession}>
-            <img
-              src="/AFB6D7AF-2CD9-4F52-8E14-55527AD561A2-removebg-preview.png"
-              alt=""
-              className="h-40"
-            />
-          </button>
+          {clickSetConfession ? (
+            <button className="mr-40 mb-5" onClick={clickSetEnd || clickSetConfession}>
+              <img
+                src="/958E0078-D2F6-48BF-8D37-9BCC4E3F39F7-fotor-bg-remover-20230422235953.png"
+                alt=""
+                className="h-48 animate-pulse"
+              />
+            </button>
+          ) : (
+            <button className="mr-40 mb-5" onClick={clickSetEnd}>
+              <img
+                src="/9CF9584A-4874-45AC-A594-74F64C2A10FF-fotor-bg-remover-20230423044.png"
+                alt=""
+                className="h-48 animate-pulse"
+              />
+            </button>
+          )}
         </div>
         <div className="bg-black opacity-80">
           {isInput ? (
             <>
               <p className="text-white text-2xl px-40 pt-4">あなた</p>
               <div className="flex items-center justify-center pb-36 mt-32">
-                <input className="h-10 opacity-60 w-1/3 outline-0" />
+                <input
+                  className="outline-0 px-2 focus:caret-white h-10 w-1/3 bg-black text-white text-2xl"
+                  placeholder={placeholder}
+                  type="text"
+                />
                 <button className="text-white border-2 py-2 px-1 ml-5" onClick={completeInput}>
-                  話しかける
+                  {talkButton}
                 </button>
               </div>
             </>
