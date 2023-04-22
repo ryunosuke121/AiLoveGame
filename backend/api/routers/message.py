e9825f8920a99c4947272fe15f482d3377e3e3f5
ryunosuke121 <ryunosuke.ito@horizon-cg.com>
2023-04-22 15:41:41 +0900

    バックエンドのmessageAPIをストリーム処理にした

--- backend/api/routers/message.py
+++ backend/api/routers/message.py
@@ -9,20 +9,22 @@ load_dotenv()
 router = APIRouter()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-#メッセージ配列を受け取って、chatGPTのストリーミングレスポンスを返す
-@router.post("/message", response_model=message.ChatResponse)
+#messages配列を受け取って、chatGPTのストリーミングレスポンスを返す
+@router.post("/message")
 async def post_message(
-    #messageBody: list[message.ChatRequest]
+    messageBody: list[message.ChatRequest]
     ):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Who won the world series in 2020?"},
-            {"role": "assistant",
-             "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            {"role": "user", "content": "Where was it played?"}
+            {'role': m.role, 'content': m.content} for m in messageBody
         ],
         stream=True
     )
-    return StreamingResponse(response)
+    def generate_stream_response():
+        for res in response:
+            if 'content' in res['choices'][0]['delta']:
+                text = res['choices'][0]['delta']['content']
+                print(text)
+                yield text
+    return StreamingResponse(generate_stream_response(), media_type="text/plain")
